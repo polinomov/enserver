f7a572910e2d59ec100e765bd58d7b37446da873
Oleg <o.tolstov1569@gmail.com>
2020-05-20 06:07:49 +0000

    Fixed Dockerfile to build succesfully Added docker-compose
    docker-compose up -d

--- app/build.sh
+++ app/build.sh
@@ -1,6 +1,6 @@
-echo --- - building --- GOPATH=$GOPATH
 # cat /etc/ld.so.conf.d/*.conf  
 BUILD_DIR=$PWD
+echo --- - building --- GOPATH=$GOPATH --BUILDIR=$BUILD_DIR
 echo --- - building protobuffer
 mkdir -p $GOPATH/src/github.com/polinomov/enserver/enbuffer/cmd 
 protoc -I=$BUILD_DIR/protodef/ --go_out=$GOPATH $BUILD_DIR/protodef/*.proto
